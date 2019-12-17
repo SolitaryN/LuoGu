cf4eee0de18eb491ca0a0493af0f9fd05bcd5f97
Solitary <2331017424@qq.com>
2019-12-17 22:12:03 +0800

    1047

--- 1047/1047.c
+++ 1047/1047.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
-int main(void)
+int main()
 {   
-    int treeSum, field;
-    scanf("%d %d", &treeSum, &field);
-    int low = 10001, high = -1, sum = 0;
-    
+    int RodeLen[10001] = {0}, field, RodeLen1;
+    scanf("%d %d", &RodeLen1, &field);
+    for(int i = 0; i < field; i++)
+    {
+        int x1, x2;
+        scanf("%d %d", &x1, &x2);
+        for(int j = x1; j <= x2; j++)
+        {
+            if(RodeLen[j] == 0)
+                RodeLen[j] = 1;
+        }
+    }
+    int sum = 0;
+    for(int i = 0; i <= RodeLen1; i++)
+        sum += RodeLen[i];
+    printf("%d", RodeLen1 - sum + 1);
     return 0;
 }
